c56b4d83f3c198a62e819d504274b5bc0bb04aa1
Ratikanta Pradhan <ratikanta@sdrc.co.in>
2020-11-18 18:05:28 +0530

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import {Map, tileLayer, marker} from 'leaflet';
+import 'leaflet';
+import 'leaflet-routing-machine';
+declare let L;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +10,20 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  map: Map;
   constructor() {}
+
+  ionViewDidEnter() {
+    this.map = new Map('map', {
+      zoomControl: false,
+      attributionControl: false
+    }).setView([20.148285, 85.881818], 12);
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+    .addTo(this.map);
+    L.Routing.control({
+      waypoints: [L.latLng(20.148285, 85.881818), L.latLng(20.2720547, 85.8463495)],
+      routeWhileDragging: true
+    }).addTo(this.map);
+  }
 
 }
